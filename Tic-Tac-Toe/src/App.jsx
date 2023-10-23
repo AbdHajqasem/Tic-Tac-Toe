b66d0a37814ca467cbb124923853490ec8d74932
Abed <abdulkareem.hajqasem@gmail.com>
2023-10-23 10:34:28 +0300

    finished enterplayername component

---
+++ Tic-Tac-Toe/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.scss";
+import EnterPlayerName from "../src/component/Enter_player_name/EnterPlayerName";
+function App() {
+  return (
+    <>
+      <EnterPlayerName />
+    </>
+  );
+}
+
+export default App;
---
+++ Tic-Tac-Toe/src/component/Enter_player_name/EnterPlayerName.jsx
@@ -0,0 +1,55 @@
+import "./EnterPlayerName.scss";
+import { useState, useRef } from "react";
+const EnterPlayerName = () => {
+  const [playersInfo, setPlayersInfo] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const playerName = useRef(null);
+  const savePlayerInfo = (event) => {
+    event.preventDefault();
+    if (!playerName.current.value) {
+      setErrorMessage("Enter your name");
+    } else if (
+      playersInfo.length == 1 &&
+      playerName.current.value == playersInfo[0].name
+    ) {
+      setErrorMessage("The name is taken by the first player");
+    } else {
+      const newPlayerInfo = {
+        id: playersInfo.length + 1,
+        name: playerName.current.value,
+      };
+      setPlayersInfo([...playersInfo, newPlayerInfo]);
+      setErrorMessage("");
+      playerName.current.value = "";
+    }
+  };
+  return (
+    <>
+      {playersInfo.length < 2 && (
+        <form onSubmit={savePlayerInfo}>
+          <div className="playersnamecontainer">
+            <h2>Player {playersInfo.length + 1}</h2>
+            <input
+              type="text"
+              className="playername"
+              ref={playerName}
+              placeholder="Enter your name"
+            />
+            <div className="errorcontainer">
+              {errorMessage && <span className="error">{errorMessage}</span>}
+            </div>
+
+            <input
+              type="submit"
+              value="Save"
+              className={
+                playersInfo.length === 0 ? "savebtnred" : "savebtnblue"
+              }
+            />
+          </div>
+        </form>
+      )}
+    </>
+  );
+};
+export default EnterPlayerName;
